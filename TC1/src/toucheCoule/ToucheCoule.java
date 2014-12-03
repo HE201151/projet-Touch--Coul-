3665f459d1a5d8f248dd09d2d2b6d9a2b44c14ce
HE201151 <romain.blan@hotmail.fr>
2014-12-03 14:31:27 +0100

    Upload Touché-Coulé
    Romain Blan, Martin Vogeleer

---
+++ TC1/src/toucheCoule/ToucheCoule.java
@@ -0,0 +1,20 @@
+/*
+ * @author Romain Blan, Vogeleer Martin
+ */
+
+
+package toucheCoule;
+
+
+import javax.swing.SwingUtilities;
+
+public class ToucheCoule {
+	public static void main(String[] args){
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				FenetreAccueil fenetre = new FenetreAccueil();
+				fenetre.setVisible(true);
+			}
+		});
+	}
+}
